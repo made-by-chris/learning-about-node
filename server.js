ffc924acefd513df8a9528460fbd7a919a4546c5
z-memory <dont@forgetmy.email>
2022-06-23 14:28:57 +0200

    basic webpage fetching data from server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
 const app = express();
-
 // DIFFERENT WAYS TO DELIVER HTML
 
 // just text
@@ -10,10 +9,26 @@ const app = express();
 // app.get("/", (req, res) => res.send("<h1>HELLO in HTML STRING</h1>"));
 
 // HTML file
-app.get("/", function (req, res) {
+app.use(express.json());
+
+app.get("/", (req, res) => {
   res.sendFile("index.html", {
     root: process.cwd(), // directory name
   });
+});
+
+app.get("/playlist", (req, res) => {
+  res.sendFile("data/database.json", {
+    root: process.cwd(), // directory name
+  });
+});
+
+app.post("/video", (req, res) => {
+  console.log(req.body);
+  // what are http status codes
+  // what are request headers?
+  // what are response headers?
+  res.send("video added to playlist");
 });
 
 app.listen(8000, () => console.log("server is running on port 8000"));
